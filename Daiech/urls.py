faf104d1ee98635f81bee5909265453dd6de599f
Mauricio Aizaga <mauricioaizaga@gmail.com>
2014-04-11 11:40:00 -0500

    Changed general structure. Added bootstrap submodule

---
+++ Daiech/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'Daiech.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^update', 'apps.github.views.update'),
+    url(r'^ads/(?P<id_ads>[-\w]+)$', 'apps.ads.views.home'),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
