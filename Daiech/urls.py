28809db911ba60d305c10d9e6ef748af5b4bcfb5
Mauricio Aizaga <mauricioaizaga@gmail.com>
2014-04-17 13:29:52 -0500

    Team and contact resonsive fixed

--- Daiech/urls.py
+++ Daiech/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Daiech.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^$', include('apps.website.urls')),
     url(r'^update', 'apps.github.views.update'),
     url(r'^ads/(?P<id_ads>[-\w]+)$', 'apps.ads.views.home'),
