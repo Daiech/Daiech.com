94916b990e3fc3f988e14ef0eef1b6effcbcc953
Mauricio Aizaga <mauricioaizaga@gmail.com>
2014-08-04 14:45:21 -0500

    Change language via GET

--- apps/website/views.py
+++ apps/website/views.py
@@ -1,9 +1,24 @@
 # Create your views here.
 from django.shortcuts import render
 from django.template import RequestContext
+from django.utils import translation
+from django.conf import settings
 
 
 def home(request):
+    if request.method == "GET" and 'lang' in request.GET:
+        try:
+            lang_available = False
+            for l in settings.LANGUAGES:
+                if request.GET.get("lang") in l:
+                    lang_available = True
+            if lang_available:
+                request.session['django_language'] = request.GET.get("lang")
+                translation.activate(request.GET.get("lang"))
+            else:
+                ctx["no_supported"] = True
+        except Exception, e:
+            print e
     return render(request, 'website/index.html', locals())
 
 
